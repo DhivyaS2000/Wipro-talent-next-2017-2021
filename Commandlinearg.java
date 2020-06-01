4e9d9717785ccfcb9bf87230031fb54823a31ed5
DhivyaS2000 <65908569+DhivyaS2000@users.noreply.github.com>
2020-06-01 19:18:50 +0530

    Add files via upload

---
+++ Commandlinearg.java
@@ -0,0 +1,11 @@
+
+public class Main
+{
+  
+    public static void main(String[] args) {
+        
+        System.out.println("100 in Binary      : "+Integer.toBinaryString(20));
+        System.out.println("100 in Hexadecimal : "+Integer.toHexString(20));
+        System.out.println("100 in Octal       : "+Integer.toOctalString(20));
+    }
+}
