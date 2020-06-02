e54c0fb362b7f4ad29b0b0ea54a590d10dba2574
DhivyaS2000 <65908569+DhivyaS2000@users.noreply.github.com>
2020-06-02 19:40:32 +0530

    Add files via upload

---
+++ Day7/Hackerrank/day4/addlastdigit.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+	public int isMultipleof(int input1,int input2)
+{
+	int rem=input1%input2;
+	if(rem!=0)
+		return 1;
+	else
+		return 2;
+}
+}
