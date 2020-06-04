e43fd1e05e8d74af6293bae8ded97f5e7222ddce
DhivyaS2000 <65908569+DhivyaS2000@users.noreply.github.com>
2020-06-04 20:13:34 +0530

    Add files via upload

---
+++ Day8/Logicalbuildingprograms/Is Palindrome.java
@@ -0,0 +1,20 @@
+ class UserMaincode {
+
+    public isPaliNum(int input1) {
+int reversedInteger = 0, remainder, originalInteger;
+
+        originalInteger = input1;
+
+        while(  input1!= 0 )
+        {
+            remainder = input1 % 10;
+            reversedInteger = reversedInteger * 10 + remainder;
+            input1 /= 10;
+        }
+
+        if (originalInteger == reversedInteger)
+            return 2;
+        else
+            return 1;
+}
+}
